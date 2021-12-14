1f4711fb1fb5d5d6cdac817c4579fd05a4fc5f7c
Skyline-23 <bs2740@naver.com>
2021-12-14 21:41:53 +0900

    [UPDATE] use horizontallyBetween

--- pinlayout-test/ViewController.swift
+++ pinlayout-test/ViewController.swift
@@ -12,18 +12,35 @@ import PinLayout
 class ViewController: UIViewController {
 
     private let view1: UIView = UIView().then {
+        $0.backgroundColor = .lightGray
+    }
+    
+    private let view2: UIView = UIView().then {
         $0.backgroundColor = .systemBlue
+    }
+    
+    private let view3: UIView = UIView().then {
+        $0.backgroundColor = .lightGray
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.addSubview(self.view1)
+        [view1, view2, view3].forEach { self.view.addSubview($0) }
     }
     
     // SafeArea, Rotate Code need to be written here
     override func viewDidLayoutSubviews() {
-        view1.pin.all(self.view.pin.safeArea)
+        self.view1.pin.centerLeft(to: self.view.anchor.centerLeft)
+            .width(100)
+            .aspectRatio(1)
+        self.view3.pin.centerRight(to: self.view.anchor.centerRight)
+            .width(100)
+            .aspectRatio(1)
+        self.view2.pin.horizontallyBetween(self.view1, and: self.view3)
+            .topLeft(to: view1.anchor.topRight)
+            .bottomRight(to: view3.anchor.bottomLeft)
+            .marginHorizontal(10)
     }
 
 }
